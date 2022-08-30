431c8b09fb00827802bef67fd54e984034e2a5de
Candlejack <garouspeak@gmail.com>
2022-08-30 10:42:36 +0300

    exercise 1 is done

---
+++ Exercise_1/exercise_1.cs
@@ -0,0 +1,17 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+using System;
+{
+    double[,] matrix = new double[3, 4];
+    Random random = new Random();
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            matrix[i, j] = random.Next(-50, 50) + random.NextDouble();
+            Console.Write("{0,6:F1}", matrix[i, j]);
+            Console.Write(" ");
+        }
+    Console.WriteLine();
+    }
+}
