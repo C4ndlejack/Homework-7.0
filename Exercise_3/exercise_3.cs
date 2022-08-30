7ca4b74df18f4af609c8ed3a9482f47343b19dc9
Candlejack <garouspeak@gmail.com>
2022-08-30 12:21:41 +0300

    exercise 3 is done

---
+++ Exercise_3/exercise_3.cs
@@ -0,0 +1,28 @@
+﻿// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+using System;
+{
+    double[,] matrix = new double[3, 4];
+    Random random = new Random();
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            matrix[i, j] = random.Next(0, 99);
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+ 
+            for (int i = 0; i < 4; i++)
+            {
+                double summ = 0;
+                for (int j = 0; j < 3; j++)
+                {
+                    summ = summ + matrix[j,i];
+                }
+                double average = summ / 3;
+                int n = i + 1;
+                Console.WriteLine("{0,6:F2}", average);
+            }
+}
